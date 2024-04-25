5667a90ba4d7569823411691eea91f1958d15f8f
azra <azra.dautt@gmail.com>
2024-04-25 16:50:11 +0200

    CORS

--- Program.cs
+++ Program.cs
@@ -1,33 +1,3 @@
-/*var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-    });
-
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();*/
-
 using Microsoft.EntityFrameworkCore;
 using PUNDERO.Models;
 
@@ -39,6 +9,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//SPAJANJE SA VSC
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReact",
+        builder =>
+        {
+            builder.WithOrigins("https://localhost:44306/", "https://localhost:3000/", "http://127.0.0.1:5500/")
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+        });
+});
 
 // Register PunderoContext with dependency injection (replace with your actual connection string name)
 builder.Services.AddDbContext<PunderoContext>(options =>
@@ -64,6 +46,9 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+//REACT
+app.UseCors("AllowReact");
 
 app.Run();
 
