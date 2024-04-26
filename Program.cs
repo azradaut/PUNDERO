7e37dfa4ed21773a2ee0edb17199582e32ab61f4
azra <azra.dautt@gmail.com>
2024-04-26 23:39:57 +0200

    Veza sa FE

--- Program.cs
+++ Program.cs
@@ -4,28 +4,28 @@ using PUNDERO.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-//SPAJANJE SA VSC
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowReact",
-        builder =>
-        {
-            builder.WithOrigins("https:/https://localhost:44306/", "https://localhost:3000/", "http://127.0.0.1:5500/")
-                   .AllowAnyHeader()
-                   .AllowAnyMethod();
-        });
-});
 
 // Register PunderoContext with dependency injection (replace with your actual connection string name)
 builder.Services.AddDbContext<PunderoContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+// CORS configuration
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        builder =>
+        {
+            builder.WithOrigins("https://localhost:44306/", "https://localhost:3000/", "http://127.0.0.1:5500/")
+            .AllowAnyHeader().
+            AllowAnyMethod();
+        });
 });
 
 var app = builder.Build();
@@ -38,17 +38,22 @@ if (app.Environment.IsDevelopment())
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
     });
-
 }
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+// Add CORS middleware after UseAuthorization
+app.UseCors("AllowReact");
+app.UseRouting();
+app.UseCors("SiteCorsPolicy");
+
+app.UseCors(builder =>
+{
+    builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+});
+
 app.MapControllers();
 
-//REACT
-app.UseCors("AllowReact");
-
 app.Run();
-
